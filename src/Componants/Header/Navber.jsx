84bcc768e80360581b7c984d44e026b598fb8d54
Anisur Rahman <anisur2004r@gmail.com>
2026-01-29 11:12:23 +0600

    fix navbar active state

--- src/Componants/Header/Navber.jsx
+++ src/Componants/Header/Navber.jsx
@@ -6,19 +6,19 @@ const Navber = () => {
         <div className=''>
             <div className='flex flex-col gap-5'>
                 <div className='flex items-center gap-2 hover:shadow-md shadow-gray-300 px-4 py-2 rounded-full duration-600 whitespace-nowrap'>
-                    <img src="/public/Image/Group (2).png" alt="" />
+                    <img src="/Image/Group (2).png" alt="" />
                         <Link to={"/"}>Dashboard Overview</Link>
                 </div>
                 <div className='flex items-center gap-2 hover:shadow-md shadow-gray-300 px-4 py-2 rounded-full  duration-600'>
-                    <img src="/public/Image/Vector (2).png" alt="" />
+                    <img src="/Image/Vector (2).png" alt="" />
                     <Link to={"calllog"}>Call Logs</Link>
                 </div>
                 <div className='flex items-center gap-2 hover:shadow-md shadow-gray-300 px-4 py-2 rounded-full  duration-600'>
-                    <img src="/public/Image/Icon.png" alt="" />
+                    <img src="/Image/Icon.png" alt="" />
                     <Link to={"appointment"}>Appointment</Link>
                 </div>
                 <div className='flex items-center gap-2 hover:shadow-md shadow-gray-300 px-4 py-2 rounded-full  duration-600'>
-                    <img src="/public/Image/Icon (1).png" alt="" />
+                    <img src="/Image/Icon (1).png" alt="" />
                     <Link className={({ isActive }) => isActive ? `` : ``} to={"settings"}>Settings</Link>
                 </div>
             </div>
